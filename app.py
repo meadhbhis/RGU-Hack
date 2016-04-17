eae14a1851b281349b60324b021f2fda1d559dfe
Meadhbh Murphy <meadhbhis@hotmail.co.uk>
2016-04-17 01:40:29 +0100

    please work in the name of the wee man

--- app.py
+++ app.py
@@ -49,10 +49,6 @@ def compare(code):
 	input_dict = {'postcode': code, 'rank': imd[code][0], 'vigintile': imd[code][1], 'population': imd[code][2], 'name': imd[code][3]}
 	return render_template('comparison.html', input=input_dict)
 
-@app.route('/compare/')
-def compare():
-    return render_template('comparison.html')
-
 @app.errorhandler(404)
 def not_found(e):
     return "Ye've daen summin wrang here pal", 404
